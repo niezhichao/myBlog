3c972a429520214692461db0522d0c5530f8d29c
niezhichao <727841296@qq.com>
2020-12-02 17:28:55 +0800

    upload logback profiles

--- myBlog_admin/src/main/java/com/nzc/blog/admin/AdminApplication.java
+++ myBlog_admin/src/main/java/com/nzc/blog/admin/AdminApplication.java
@@ -1,6 +1,7 @@
 package com.nzc.blog.admin;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
