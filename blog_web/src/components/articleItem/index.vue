6cf11c784fa10cc5472bcbaa49a1d2c2b25298af
niezhichao <727841296@qq.com>
2020-12-15 23:31:05 +0800

    ada d

--- blog_web/src/components/articleItem/index.vue
+++ blog_web/src/components/articleItem/index.vue
@@ -3,7 +3,9 @@
 
       <div style="height: 100%">
         <el-container>
-          <el-header></el-header>
+          <el-header>
+              
+          </el-header>
           <el-main></el-main>
           <el-footer></el-footer>
         </el-container>
@@ -14,6 +16,14 @@
 <script>
     export default {
         name: "articleItem",
+      data(){
+          return {
+            articleIntros:[{
+                 title:"Vue项目开发常见问题和解决方案汇总（持续整理）",
+                 intro:"Vue Cli 打包之后静态资源路径不对的解决方法"
+            }]
+          }
+      }
 
     }
 </script>
